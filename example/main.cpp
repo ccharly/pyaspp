857433ad31f05e69211531fa4dad70e834cf0c5a
Charly Chevalier <charly.n.chevalier@gmail.com>
2015-06-29 23:44:43 +0200

    Update example

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,20 @@
 #py= cpp.define("salut", "LOL")
 
+#py= cpp.define('ONE_AND_ONE', 1 + 1)
+
+#py a_python_value = ''.join(['py', 'thon'])
+#py= cpp.define('PYTHON', a_python_value)
+
+#py{
+
+def foo():
+    for i in range(10):
+        yield cpp.define('ITER' + str(i), i)
+
+#py}
+
+#py= foo()
+
 int main() {
     return 0;
 }
